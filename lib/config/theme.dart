1d3549da3e4048ab79477cdb94ab15206fb37112
Diego Armando <fordiego44@gmail.com>
2023-06-14 16:55:35 -0500

    Modifcamos estilos de appbar

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -24,7 +24,7 @@ class AppTheme {
     useMaterial3: true,
     colorSchemeSeed: colorList[selectedColor],
     appBarTheme: const AppBarTheme(
-      centerTitle: true
+      centerTitle:true
     )
   );
 }
