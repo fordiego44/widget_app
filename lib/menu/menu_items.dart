4795eef30307113b61ba66d3609c5e91737da93f
Diego Armando <fordiego44@gmail.com>
2023-06-15 08:45:18 -0500

    rutas básicas configuradas

--- lib/menu/menu_items.dart
+++ lib/menu/menu_items.dart
@@ -24,7 +24,7 @@ const List<MenuItem> appMenuitems = [
   MenuItem(
     title: 'Tarjetas',
     subTitle: 'Un contenedor utilizado', 
-    link: '/card', 
+    link: '/cards', 
     icon: Icons.credit_card 
   ),
 ];
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -28,7 +29,7 @@ class _HomeView extends StatelessWidget {
 }
 
 class _CustomListTile extends StatelessWidget {
-  const _CustomListTile({ 
+  const _CustomListTile({
     required this.menuItem,
   });
 
@@ -36,7 +37,6 @@ class _CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
@@ -44,8 +44,14 @@ class _CustomListTile extends StatelessWidget {
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap:(){
+      onTap: () {
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // );
 
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
